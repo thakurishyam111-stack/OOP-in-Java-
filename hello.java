fa85179800ace987032824c69b8cfe9d48f7b61f
Shyam Shah Thakuri <thakurishyam111@gmail.com>
2026-01-07 07:06:33 +0545

    sum

---
+++ hello.java
@@ -0,0 +1,10 @@
+public class hello{
+    public static void main(String[] args) {
+        int a=10;
+        int b=20;
+       int  sum=0;
+         sum = a+b;
+
+        System.out.println(sum );
+    }
+}
