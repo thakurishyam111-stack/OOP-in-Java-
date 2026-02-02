43c18c27e8542da5333ec76426aa9369b88f6109
Shyam Shah Thakuri <thakurishyam111@gmail.com>
2026-02-02 18:40:43 +0545

    loop in java

--- Loop/level1.java
+++ Loop/level1.java
@@ -1,23 +1,19 @@
 package Loop;
 
-// public class level1 {
-//     public static void main(String[] args) {
-//         int i=0;
-//         do{
-//         System.out.println(i);
-
-//             i++;
-            
-//         }
-//         while(i<=10);
-
-
-//     }
-// }
 public class level1 {
     public static void main(String[] args) {
-
         int i=0;
+        do{
+        System.out.println(i);
+
+            i++;
+            
+        }
+        while(i<=10);
+
+
+
+        
         while(i<=10){
             System.out.println(i);
             i++;
